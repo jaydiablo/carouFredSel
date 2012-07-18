bcb5099e20b809bb714090ac28bc5d0c8082c356
Jay Klehr <jay@diablomedia.com>
2012-07-17 18:24:03 -0600

    another fix to refresh

--- jquery.carouFredSel-5.6.2.js
+++ jquery.carouFredSel-5.6.2.js
@@ -1498,6 +1498,11 @@ if (opts.responsive) {
             $cfs.bind(cf_e('refresh', conf), function(e) {
                 e.stopPropagation();
                
+                itms.total = $cfs.children().length;
+                var sz = $cfs.triggerHandler('updateSizes');
+                nv_showNavi(opts, itms.total, conf);
+                nv_enableNavi(opts, itms.first, conf);
+                $cfs.trigger(cf_e('updatePageStatus', conf), [true, sz]);
                 $cfs.trigger(cf_e('configuration', conf), conf, true);
 
                 return true; 
