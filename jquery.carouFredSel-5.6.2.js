70ef99e06b33ca2e1932190dc450d465d07b9d79
Jay Klehr <jay@diablomedia.com>
2012-07-17 18:25:47 -0600

    removing code that is duplicate (configuration runs this)

--- jquery.carouFredSel-5.6.2.js
+++ jquery.carouFredSel-5.6.2.js
@@ -1502,7 +1502,6 @@ if (opts.responsive) {
                 var sz = $cfs.triggerHandler('updateSizes');
                 nv_showNavi(opts, itms.total, conf);
                 nv_enableNavi(opts, itms.first, conf);
-                $cfs.trigger(cf_e('updatePageStatus', conf), [true, sz]);
                 $cfs.trigger(cf_e('configuration', conf), conf, true);
 
                 return true; 
